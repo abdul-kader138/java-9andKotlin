3c30d5578a6f6d1d655644220a4fcdc32c770fb1
abdul-kader138 <babu313136@gmail.com>
2017-09-25 12:51:32 +0600

    Kotlin(Array Declaration)

--- src/com/company/B_K.kt
+++ src/com/company/B_K.kt
@@ -1,9 +1,17 @@
 package com.company
 
 
-
-
 fun main(args: Array<String>) {
-    val a=4;
-    println(a);
+
+
+    var testArray= arrayOf('0','1','2','3','4','5','6','7').toCharArray();
+    println(decimalDigitValue('0',testArray));
+
+}
+
+
+fun decimalDigitValue(c: Char, d:CharArray): Int {
+    if (c !in d)
+        throw IllegalArgumentException("Out of range")
+    return c.toInt() - '0'.toInt() // Explicit conversions to numbers
 }
